3e69a9e99b046002bafe2c4b1ad8adda6af6bf90
dyh_a <xinzon32177@163.com>
2022-09-07 00:04:41 +0800

    feat: vue环境搭建

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 // import { resolve } from 'path';
 import { defineConfig, UserConfigExport } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(({ mode }) => {
   const config: UserConfigExport = {
+    plugins: [vue()],
     resolve: {
       alias: {
         // '@mxssfd': resolve(__dirname, 'packages'),
